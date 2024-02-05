938be2d36f6f3ab1b8d0eb622f4797b501821f43
부울경_1반_어지민 <eojm9388@gmail.com>
2024-02-05 14:07:17 +0900

    4861_문자열비교 문제풀이

--- 04_String/4861_문자열_비교/sol.py
+++ 04_String/4861_문자열_비교/sol.py
@@ -1,3 +1,14 @@
 import sys
 sys.stdin = open('input.txt')
 
+T = int(input())
+
+for tc in range(1, T+1):
+    str1 = input()
+    str2 = input()
+
+    print(f'#{tc}', end=' ')
+    if str1 in str2:
+        print(1)
+    else:
+        print(0)
