b3afcc07c666fe1775b530f897c6809af3c55407
부울경_1반_어지민 <eojm9388@gmail.com>
2024-02-15 10:06:45 +0900

    4869_종이붙이기 문제풀이

--- 07_Stack_I/4869_종이붙이기/sol.py
+++ 07_Stack_I/4869_종이붙이기/sol.py
@@ -1,3 +1,16 @@
-import sys
-sys.stdin = open('input.txt')
+# import sys
+# sys.stdin = open('input.txt')
 
+T = int(input())
+
+for tc in range(1, T+1):
+    N = int(input())//10
+    memo = [0] * (N+1)
+    memo[1] = 1
+    memo[2] = 3
+
+    # 점화식
+    for i in range(3, N+1):
+        memo[i] = memo[i-1] + 2*memo[i-2]
+
+    print(f'#{tc}', memo[N])
