d03afb01c9e83f380388874ccc263bd1f738daac
부울경_1반_어지민 <eojm9388@gmail.com>
2024-02-22 17:55:47 +0900

    암호코드 푸는중

--- 21_Start/1242_암호코드_스캔/sol.py
+++ 21_Start/1242_암호코드_스캔/sol.py
@@ -1,2 +1,55 @@
 import sys
 sys.stdin = open('input.txt')
+
+T = int(input())
+
+BIN = [format(i, 'b').zfill(4) for i in range(17)]
+END_BIN = [i.rstrip('0') for i in BIN]
+# print(BIN)
+# print(END_BIN)
+def make_code(start, count, line, after):
+    new_line = line
+    after_code = new_line[start] + after
+    if count > 1:
+        new_code = BIN[int(line[start], 16)]
+    else:
+        new_code = END_BIN[int(line[start], 16)]
+    start -= 1
+    while len(new_code) < 56:
+        after_code = new_line[start] + after_code
+        code_one = BIN[int(line[start], 16)]
+        new_code = code_one + new_code
+        start -= 1
+    if new_line[start-1] != '0':
+        new_code = make_code(start-1, count+1, new_line, after_code) + new_code
+        return new_code, after_code
+    else:
+        return new_code, after_code
+#
+#
+
+def make_after(code, after):
+    after_code = code.replace(after, '').rstrip('0')
+    return after_code
+for tc in range(1, T+1):
+    print(tc)
+    N, M = map(int, input().split())
+    code_line = []
+    for n in range(N):
+        line = input().rstrip('0')
+        s = len(line)
+        if s:
+            code, after = make_code(-1, 1, line, '')
+            line = make_after(line, after)
+            print(line)
+            while len(line):
+                continue
+
+
+            k = len(code) % 56
+
+            # print(code[k:])
+
+
+    # print(code_line)
+
