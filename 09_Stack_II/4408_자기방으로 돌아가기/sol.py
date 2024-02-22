54c7a9312c425a34c1c759f12dbdd207331b3f1b
부울경_1반_어지민 <eojm9388@gmail.com>
2024-02-22 13:59:55 +0900

    5185_이진수 문제풀이

--- 09_Stack_II/4408_자기방으로 돌아가기/sol.py
+++ 09_Stack_II/4408_자기방으로 돌아가기/sol.py
@@ -1,3 +1,32 @@
-import sys
-sys.stdin = open("input.txt")
+# import sys
+# sys.stdin = open("input.txt")
 
+T = int(input())
+
+for tc in range(1, T+1):
+    N = int(input())
+    start_room = [2]
+    end_room = [400]
+
+    result = []
+    for i in range(N):
+        count = 0
+        start, end = map(int, input().split())
+        if start % 2 == 1:
+            start += 1
+        if end % 2 == 1:
+            end += 1
+
+        for j in range(i+1):
+            if end_room[j] == start:
+                continue
+            elif start_room[i] == end:
+                continue
+            if start_room[j] <= start <= end_room[j]:
+                count += 1
+
+        result.append(count)
+        start_room.append(start)
+        end_room.append(end)
+
+    print(f'#{tc}', max(result))
